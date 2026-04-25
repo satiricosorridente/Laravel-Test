5977a5ff9703e7dfb350af328393cd48367af9d0
Samuel Dayne <samuel.dayne2008@gmail.com>
2026-04-25 18:43:12 -0300

    feat: Teste Inicial

---
+++ l11/storage/framework/views/3ae57b4f86e2660300a70631e22d02d5.php
@@ -0,0 +1,7 @@
+<?php if($response): ?>
+<?php echo $response->head; ?>
+
+<?php else: ?>
+<?php echo $slot; ?>
+
+<?php endif; ?><?php /**PATH C:\Users\Usuario\Herd\l11\storage\framework\views/58f6b5ecbad88fc4da15879dbc7f6e69.blade.php ENDPATH**/ ?>
